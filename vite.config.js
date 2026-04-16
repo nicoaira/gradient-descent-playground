1f300367222b14f42340eeeda6281204e53a141f
Nicolas <aira.nicolas94@gmail.com>
2026-04-16 17:38:40 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-})  
+    base: '/gradient-descent-playground/' // Set to match your GitHub repository name
+})
